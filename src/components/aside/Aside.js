f65f8dc8890b08ea66e080f85468e9c8c35836dc
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-20 00:50:09 +0300

    fix: sidebar styling fixes

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,40 +1,44 @@
 import React from 'react'
-import { MdSpaceDashboard,MdAdminPanelSettings,MdAnalytics, MdOutlineAccessTimeFilled } from "react-icons/md";
+import { MdSpaceDashboard,MdAdminPanelSettings,MdAnalytics, MdOutlineAccessTimeFilled, MdOutlineClose } from "react-icons/md";
 import { IoTimer } from "react-icons/io5";
+import { RiLogoutCircleLine } from "react-icons/ri";
 function Aside() {
   return (
     <aside>
-        <div className="logo">
-        <IoTimer className='logo__icon'/>
-        <h2>Clock-Me</h2>
+        <div className='top'>
+            <div className="logo">
+            <IoTimer className='logo__icon'/>
+            <h2>Clock-Me</h2>
+            </div>
+            <div className='close'>
+                <MdOutlineClose />
+            </div>
         </div>
         <nav className='side-bar'>
-        <ul>
-            <li>
-                <a href="/">
-                    <MdSpaceDashboard />
-                    <h3>Dashboard</h3>
-                </a>
-            </li>
-            <li>
+            <a href="/">
+                <MdSpaceDashboard />
+                <h3>Dashboard</h3>
+            </a>
+            
             <a href="/" className='active'>
                 <MdAdminPanelSettings />
                 <h3>Admin</h3>
             </a>
-        </li>
-        <li>
-        <a href="/">
-            <MdAnalytics />
-            <h3>Analytics</h3>
-        </a>
-    </li>
-    <li>
-    <a href="/">
-    <MdOutlineAccessTimeFilled />
-        <h3>Timecard</h3>
-    </a>
-</li>
-        </ul>
+        
+            <a href="/">
+                <MdAnalytics />
+                <h3>Analytics</h3>
+            </a>
+    
+   
+            <a href="/">
+            <MdOutlineAccessTimeFilled />
+                <h3>Timecard</h3>
+            </a>
+            <a href="/">
+            <RiLogoutCircleLine />
+                <h3>Logout</h3>
+            </a>
         </nav>
 
     </aside>
