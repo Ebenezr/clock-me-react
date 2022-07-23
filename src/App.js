8e0d2491a6c71efb0b26a02cb57309f738feffe9
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-23 19:46:38 +0300

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom"
-import Aside from './components/aside/Aside';
-import Main from './components/main/Main';
-import About from './components/about/About';
-import Login from './components/Login/Login'
-import Dashboard from './components/main/Dashboard';
-import Timecard from './components/main/Timecard';
-import Admin from './components/main/Admin';
-import Analytics from './components/main/Analytics';
+import React from "react";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import Aside from "./components/aside/Aside";
+import Main from "./components/main/Main";
+import About from "./components/about/About";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/main/Dashboard";
+import Timecard from "./components/main/Timecard";
+import Admin from "./components/main/Admin";
+import Analytics from "./components/main/Analytics";
 
-
-import './components/scss/style.scss';
+import "./components/scss/style.scss";
 
 const App = () => {
   return (
-    <Login />
+    <main className="App">
+      <Aside />
+      <Main />
+    </main>
   );
-}
+};
 
 export default App;
-{/* <Routes>
+{
+  /* <Routes>
 <Route path="login" element={<Login />}/>
 <Route path="about" element={<About />}/>
 <Route path="home" element={<Main />}>
@@ -28,4 +31,5 @@ export default App;
     <Route path="analytics" element={<Analytics />}/>
     <Route path="timecard" element={<Timecard />}/>
 </Route>
-</Routes> */}
+</Routes> */
+}
