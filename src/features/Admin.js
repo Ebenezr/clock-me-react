1f0a48df1761a9c950154c382646f4cb2a7d4fd4
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-25 00:35:25 +0300

    add admin panel structure and placeholder mockup

--- src/features/Admin.js
+++ src/features/Admin.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import profile from "../components/assets/images/ebe.png";
 
-function Admin() {
+const Admin = () => {
   return (
-    <div className='admin__view'>Admin</div>
-  )
-}
+    <section className="admin__view">
+      <div className="left">
+        <form>
+          <BiSearchAlt className="search__icon" />
+          <input className="search" type="search" placeholder="Type away..." />
+        </form>
+        <div className="welcome-info">
+          <h2>Hello Ebenezar</h2>
+          <span>Welcome back!</span>
+        </div>
+        <div className="users-list">
+          <div className="user-title">
+            <h3>Manage Employees</h3>
+            <select type="option">
+              <option>Filter</option>
+              <option>System Design</option>
+              <option>Sales</option>
+            </select>
+          </div>
+          <span>
+            <h4>Jon Doe</h4>
+            <small>Sales</small>
+          </span>
+          <span>
+            <h4>Mac Lui</h4>
+            <small>Sales</small>
+          </span>
+          <span>
+            <h4>Jasmin Iloe</h4>
+            <small>Support</small>
+          </span>
+          <span>
+            <h4>React ni Ngumu</h4>
+            <small>HR</small>
+          </span>
+        </div>
+      </div>
+      <div className="right">
+        <div className="card">
+          <img src={profile} />
+          <h3>Jon Doe</h3>
+          <span className="card-info">
+            <span>Usertype</span>
+            <p>Admin</p>
+          </span>
+          <span className="card-info">
+            <span>StaffId</span>
+            <p>SD-456</p>
+          </span>
+          <span className="card-info">
+            <span>Department</span>
+            <p>Support Desk</p>
+          </span>
+        </div>
+        <div className="right-manage">
+          <button className="btn-update">Update Info</button>
+          <button className="btn-new">Add New?</button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Admin
+export default Admin;
--- src/features/Main.js
+++ src/features/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -6,8 +6,17 @@ import Dashboard from "./Dashboard";
 import Admin from "./Admin";
 import Analytics from "./Analytics";
 import Timecard from "./Timecard";
+import axios from "../api/axios";
 
 const Main = () => {
+  const [users, setUsers] = useState([]);
+  const url = "http://localhost:8004/users";
+
+  const fetchUsers = () => {
+    axios.get(url).then((responce) => (allusers = responce.data));
+    setUsers(allusers);
+  };
+
   return (
     <section className="container__main">
       <Header />
