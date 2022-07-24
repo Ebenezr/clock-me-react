98054a6c5d1573491161c03dacc7e21f47abadcd
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-25 00:46:55 +0300

    minor fixes

--- src/features/Main.js
+++ src/features/Main.js
@@ -12,10 +12,10 @@ const Main = () => {
   const [users, setUsers] = useState([]);
   const url = "http://localhost:8004/users";
 
-  const fetchUsers = () => {
-    axios.get(url).then((responce) => (allusers = responce.data));
-    setUsers(allusers);
-  };
+  // const fetchUsers = () => {
+  //   axios.get(url).then((responce) => (allusers = responce.data));
+  //   setUsers(allusers);
+  // };
 
   return (
     <section className="container__main">
